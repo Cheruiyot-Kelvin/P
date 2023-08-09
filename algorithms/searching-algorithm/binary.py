20670721de388939cedc8f0790b72f2fa8325cd5
G3NTL3M4N <kelvincheruiyot000@gmail.com>
2023-08-10 00:06:44 +0300

    Binary Search Algorithm

---
+++ algorithms/searching-algorithm/binary.py
@@ -0,0 +1,14 @@
+import bisect
+def binary_search_bisect(arr, x):
+	i = bisect.bisect_left(arr, x)
+	if i != len(arr) and arr[i] == x:
+		return i
+	else:
+		return -1
+arr = [2, 3, 4, 10, 40]
+x = 55
+result = binary_search_bisect(arr, x)
+if result != -1:
+	print("Element is present at index", str(result))
+else:
+	print("Element is not present in array")
